4167a7a98761931bc6bbbf0ec7106976fd0a121c
sudeep500 <sudipta13.chakraborty@gmail.com>
2023-12-12 22:46:33 +0530

    boilerplate server setup

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(express.urlencoded({ extended: false }));
+
+const User = mongoose.model('User', {
+  frist_name: String,
+  last_name: String,
+  email: String,
+  avatar: String,
+});
+
+app.get('/', (req, res) => {
+  res.json({ status: 'SUCCESS', message: 'All Good' });
+});
+
+app.listen(process.env.PORT_NO, () => {
+  mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log(`Server is listening on port:${process.env.PORT_NO}...`))
+    .catch((error) => console.log(error));
+})
